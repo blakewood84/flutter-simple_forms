90924d53b95a5a5f7b2ae233242fef57aa814893
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-08-01 15:26:04 -0700

    small update

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Forms Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
   }
